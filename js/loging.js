cd419ecebb96b44f77627e1d2fe620931fb0623e
Matheus carolino <118949040+MMatheus3dsc@users.noreply.github.com>
2024-01-09 10:30:33 -0300

    validações dos campos de email/senha
    validação basica via js

--- js/loging.js
+++ js/loging.js
@@ -1,3 +1,40 @@
-let email = document.querySelector("email");
-let senha = document.querySelector("senha");
-let btn = document.querySelector("btn")
+let email = document.querySelector("#email");
+let senha = document.querySelector("#senha");
+let btn = document.querySelector("#btn");
+let form = document.querySelector("#form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    if(email.value === "" || !validEmail(email.value)){
+    alert("Preencha seu Email");
+    return;
+  }
+  if(!validSenha(senha.value,8)){
+    alert("a senha prescisa no mínimo 8 dígitos");
+    return;
+  }
+
+
+    form.submit();
+})
+
+function validEmail (email){
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    ); // regex que permite letras maisculas e minusculas e numeros,  caracteres especias
+
+    if(emailRegex.test(email)){
+        return true;
+    }else{
+        return false;
+    }
+    
+}
+function validSenha(senha,minDigitos){
+if(senha.length >= minDigitos){
+     return true;
+}else{
+    return false;
+}
+}
