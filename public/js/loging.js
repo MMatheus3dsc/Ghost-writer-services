e623c64d2a9e6273abc86ede6fccadabb79989fd
Matheus carolino <118949040+MMatheus3dsc@users.noreply.github.com>
2024-03-19 17:01:30 -0300

    localhost
    um servidor local pra testa melhor as aplicações com empress, e uma melhorada nas verificações de login

---
+++ public/js/loging.js
@@ -0,0 +1,44 @@
+const email = document.querySelector("#email");
+const senha = document.querySelector("#senha");
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    if (email.value === "" || !validEmail(email.value)) {
+      alert("Preencha seu Email");
+    } else if (!validSenha(senha.value, 8)) {
+      alert("a senha prescisa no mínimo 8 dígitos");
+    } else {
+      setTimeout(function() {
+        window.location.href = "/html/index.html"; 
+      }, 1000);
+    }
+  });
+  
+
+
+function validEmail (email){
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    ); // regex que permite letras maisculas e minusculas e numeros,  caracteres especias
+
+    if(emailRegex.test(email)){
+        return true;
+    }else{
+        return false;
+    }
+    
+}
+function validSenha(senha,minDigitos){
+if(senha.length >= minDigitos){
+     return true;
+}else{
+    return false;
+}
+}
+
+export const module = {
+    validEmail,
+    validSenha
+}
